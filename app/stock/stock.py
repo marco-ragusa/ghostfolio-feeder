08335b6bb73da10097d5e8a1fdade9ff8f90905b
marco-ragusa <>
2024-02-12 22:15:24 +0100

    Added docstring to stock.py

--- app/stock/stock.py
+++ app/stock/stock.py
@@ -7,6 +7,15 @@ from .local import local
 
 
 class Stock:
+    """
+    Represents a stock and provides methods to fetch data from different sources.
+
+    Attributes:
+        ticker (str): The ticker symbol of the stock.
+        start_date (str): The start date for fetching data (optional).
+        end_date (str): The end date for fetching data (optional).
+    """
+
     def __init__(
             self, ticker: str, start_date: str | None = None, end_date: str | None = None
         ) -> None:
@@ -18,21 +27,27 @@ class Stock:
 
 
     def mvis(self) -> list:
+        """Fetches stock data from MVIS."""
         return mvis(self.ticker, self.start_date, self.end_date)
 
     def corriere(self) -> list:
+        """Fetches stock data from Corriere."""
         return corriere(self.ticker, self.start_date, self.end_date)
 
     def byblos(self) -> list:
+        """Fetches stock data from Byblos."""
         return byblos(self.ticker, self.start_date, self.end_date)
 
     def boerse_frankfurt(self) -> list:
+        """Fetches stock data from Börse Frankfurt."""
         return boerse_frankfurt(self.ticker, self.start_date, self.end_date)
 
     def fondofonte(self) -> list:
+        """Fetches stock data from Fondofonte."""
         return fondofonte(self.ticker, self.start_date, self.end_date)
 
     def local(self) -> list:
+        """Fetches stock data from local source."""
         return local(self.ticker, self.start_date, self.end_date)
 
 

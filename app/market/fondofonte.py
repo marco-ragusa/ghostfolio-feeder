e07a339fc8905422219b9055a95bc0189ec658c9
marco-ragusa <>
2024-05-08 16:03:58 +0200

    Improved fondofonte data source

--- app/market/fondofonte.py
+++ app/market/fondofonte.py
@@ -1,3 +1,4 @@
+"""Fon.Te. module."""
 from bs4 import BeautifulSoup
 import requests
 # Import utils
@@ -7,46 +8,25 @@ except ImportError:
     import utils
 
 
-def fondofonte(ticker: str, start_date: str | None = None, end_date: str | None = None) -> list:
+def fondofonte_scraper(html: str) -> list:
     """
-    Fetches historical market data for a given ticker symbol from Fondo Fon.Te web page.
+    Scrape market data from HTML content obtained from Fondo Fon.Te webpage.
 
     Args:
-        ticker (str): The ticker symbol of the financial instrument.
-        start_date (str, optional): The start date for data retrieval (YYYY-MM-DD format).
-        end_date (str, optional): The end date for data retrieval (YYYY-MM-DD format).
+        html (str): The HTML content of the webpage.
 
     Returns:
-        list: A list of dictionaries containing historical market data in the following format:
-            {'date': 'yyyy-mm-dd', 'marketPrice': int}
+        list: A list of dictionaries containing market data in the following format:
+            [{'date': 'yyyy-mm-dd', 'marketPrice': str}]
     """
-
-    # Get web page
-    base_url = f'https://www.fondofonte.it/gestione-finanziaria/i-valori-quota-dei-comparti/{ticker}/'
-    headers = {
-        'User-Agent': utils.get_random_user_agent(),
-        'Referer': base_url,
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
-    }
-    # Make an HTTP request and get the content of the page
-    response = requests.get(
-        base_url,
-        headers=headers,
-        timeout=10,
-    )
-    html = response.text
+    # Create a dictionary to store the information
+    market_data = []
 
     # Create the BeautifulSoup object
     soup = BeautifulSoup(html, 'html.parser')
 
     # Find all elements with the class 'toggle-acf'
     toggle_elements = soup.find_all(class_='toggle-acf')
-
-    # Create a dictionary to store the information
-    market_data = []
 
     # Iterate through all toggle elements
     for toggle in toggle_elements:
@@ -62,15 +42,53 @@ def fondofonte(ticker: str, start_date: str | None = None, end_date: str | None 
         # Iterate through all quote information rows
         for quote_row in quote_rows:
             # Extract the date and quote from the row
-            date = quote_row.find('span', {'class': None}).text.strip()
-            if date in 'Periodo':
+            cells = quote_row.find_all('span', {'class': None})
+            date = cells[0].text.strip()
+            if date == 'Periodo':
                 continue
-            quote = quote_row.find_all('span', {'class': None})[1].text.strip()
+            quote = cells[1].text.strip()
 
             # Add the information to the list
             market_data.append({'date': f'{date} {year}', 'marketPrice': quote})
 
-    # Format market data in this way {'date': 'yyyy-mm-dd', 'marketPrice': int}
+    return market_data
+
+
+def fondofonte(ticker: str, start_date: str | None = None, end_date: str | None = None) -> list:
+    """
+    Fetches historical market data for a given ticker symbol from Fondo Fon.Te web page.
+
+    Args:
+        ticker (str): The ticker symbol of the financial instrument.
+        start_date (str, optional): The start date for data retrieval (YYYY-MM-DD format).
+        end_date (str, optional): The end date for data retrieval (YYYY-MM-DD format).
+
+    Returns:
+        list: A list of dictionaries containing historical market data in the following format:
+            {'date': 'yyyy-mm-dd', 'marketPrice': float}
+    """
+    # Get web page
+    url = f'https://www.fondofonte.it/gestione-finanziaria/i-valori-quota-dei-comparti/{ticker}/'
+    headers = {
+        'User-Agent': utils.get_random_user_agent(),
+        'Referer': url,
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+    }
+    # Make an HTTP request and get the content of the page
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=10,
+    )
+    html = response.text
+
+    # Extract market data from webpage
+    market_data = fondofonte_scraper(html)
+
+    # Format market data in this way {'date': 'yyyy-mm-dd', 'marketPrice': float}
     market_data = [{
         "marketPrice": float(item["marketPrice"].replace(',','.')),
         "date": utils.convert_italian_date(item["date"])
@@ -82,5 +100,10 @@ def fondofonte(ticker: str, start_date: str | None = None, end_date: str | None 
     return market_data
 
 
-if __name__ == "__main__":
+def main():
+    """Main function for local tests only."""
     utils.print_list(fondofonte("comparto-dinamico", start_date="2023-10-19"))
+
+
+if __name__ == "__main__":
+    main()

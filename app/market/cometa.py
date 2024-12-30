bace12cac528b71a8cd5394081edb1652caad1e3
marco-ragusa <macroragusa@gmail.com>
2024-12-30 22:50:30 +0100

    Fixed cometa scraping

--- app/market/cometa.py
+++ app/market/cometa.py
@@ -1,4 +1,5 @@
 """Cometa module."""
+from datetime import datetime
 import re
 from bs4 import BeautifulSoup, Comment
 import requests
@@ -18,30 +19,30 @@ def cometa_scraper(html: str) -> list:
 
     Returns:
         list: A list of dictionaries containing market data in the following format:
-            [{'date': 'yyyy-mm-dd', 'marketPrice': str}]
+            [{'date': 'mm/yyyy', 'marketPrice': str}]
     """
-    # Create a dictionary to store the information
-    market_data = []
-
     # Parsing HTML
     soup = BeautifulSoup(html, 'html.parser')
 
-    # Find all comments in HTML
-    comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+    # Find the table with id="table_2" and get the data-wpdatatable_id attribute
+    table_2 = soup.find('table', id="table_2")
+    if not table_2 or not table_2.has_attr('data-wpdatatable_id'):
+        raise Exception("Table with id='table_2' or data-wpdatatable_id attribute not found.")
 
-    # Regex to extract date and price
-    pattern = re.compile(r"new Date\((\d{4}),(\d{2}),(\d{2}),.*\),\s+([\d.]+)\s+\]")
+    wpdatatable_id = table_2['data-wpdatatable_id']
 
-    # Loop through the comments
-    for comment in comments:
-        match = pattern.search(comment)
-        if match:
-            year = match.group(1)
-            month = int(match.group(2))+1 # +1 Because JS sucks
-            day = match.group(3)
-            price = match.group(4)
-            date_str = f"{year}-{month}-{day}"
-            market_data.append({'date': date_str, 'marketPrice': price})
+    # Regular expression to match the id of <tr>
+    id_pattern = re.compile(f"^table_{wpdatatable_id}_row_\d+$")
+
+    # Find all <tr> elements matching the criteria
+    rows = soup.find_all('tr', id=id_pattern, attrs={'data-row-index': True})
+
+    # Extract the first two <td> elements of each <tr>
+    market_data = []
+    for row in rows:
+        tds = row.find_all('td')
+        if len(tds) >= 2:
+            market_data.append({'date': tds[0].get_text(strip=True), 'marketPrice': tds[1].get_text(strip=True)})
 
     return market_data
 
@@ -75,6 +76,7 @@ def cometa(
     # Make an HTTP request and get the content of the page
     response = requests.get(
         url,
+        verify=False,
         headers=headers,
         timeout=10,
     )
@@ -85,8 +87,10 @@ def cometa(
 
     # Format market data in this way {'date': 'yyyy-mm-dd', 'marketPrice': int}
     market_data = [{
-        "marketPrice": float(item["marketPrice"]),
-        "date": item["date"]
+        "marketPrice": float(item["marketPrice"].replace(',','.')),
+        "date": datetime.strptime(
+            item["date"], "%m/%Y"
+        ).strftime("%Y-%m-%d")
     } for item in market_data]
 
     # Fill missing dates
@@ -97,7 +101,7 @@ def cometa(
 
 def main() -> None:
     """Main function for local tests only."""
-    utils.print_list(cometa("reddito", "2000-01-01"))
+    utils.print_list(cometa("crescita", "2000-01-01"))
 
 
 if __name__ == "__main__":
